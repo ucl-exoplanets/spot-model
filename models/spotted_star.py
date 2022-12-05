d85bc8754f30014023a8bb25c076b2de81214090
mariomorvan <mario.morvan.18@ucl.ac.uk>
2022-12-05 12:51:39 +0000

    method to show star

--- models/spotted_star.py
+++ models/spotted_star.py
@@ -2,6 +2,7 @@ import warnings
 
 import numpy as np
 import matplotlib.pylab as plt
+from matplotlib.colors import LinearSegmentedColormap
 
 
 class BaseStarModel(object):
@@ -29,8 +30,40 @@ class BaseStarModel(object):
         self.Ph = np.arcsin(self.Y / np.sin(self.Th))
         self.X = np.sin(self.Th) * np.cos(self.Ph)
 
-    def show(self):
-        raise NotImplementedError
+    def show(self, spotted_mask,  yp=None, zp=None, rp=None, ax=None, axis=False,
+             cm=LinearSegmentedColormap.from_list("custom", ["#fff305", "#ffa805"])):
+        if ax is None:
+            _, ax = plt.subplots()
+        plt.pcolormesh(self.Y, self.Z, spotted_mask,
+                       shading='nearest', cmap=cm, antialiased=True)  # 'YlGn', vmin=0, vmax=3)
+        plt.axhline(0, color='black', label='equator',
+                    linestyle='dashed', linewidth=0.7)
+        ax.add_patch(plt.Circle((0, 0), 1, edgecolor='black',
+                     facecolor='none', linewidth=0.4))
+
+        # Show planet path if yp is provided
+        if yp is not None:
+            if hasattr(rp, '__len__'):
+                warnings.warn(
+                    'only one radius is supported, taking the largest radius')
+                rp = np.max(rp)
+            if not hasattr(yp, '__len__'):
+                yp = [yp]
+                assert not hasattr(zp, '__len__')
+                zp = [zp]
+            else:
+                assert len(yp) == len(zp)
+                list_alpha = np.linspace(0.5, 1, len(yp))
+            for i in range(len(zp)):
+                ax.add_patch(plt.Circle((yp[i], zp[i]), rp, edgecolor=None,
+                                        facecolor='black', alpha=list_alpha[i]))
+        plt.axis('equal')
+        if not axis:
+            plt.axis('off')
+        else:
+            plt.xlabel('Y')
+            plt.ylabel('Z', rotation=0)
+        return ax
 
 
 def spher_to_cart(lat, lon):
@@ -120,27 +153,27 @@ class StarModel(BaseStarModel):
         # planet mask for various radii, and indices of the polar rectangle surrounding the largest radius
         mask_p, indr_p, indtheta_p = self.create_mask_planet(y0p, z0p, rplanet)
 
-        #### planet integration
+        # planet integration
         fraction_planet = np.zeros((len(self.radii), nw))
         fraction_planet[indr_p] = ((mask_p/2).sum(0)*self.deltath)/(2.*np.pi)
         ff_planet = np.sum(fraction_planet * 2. * np.pi *
                            self.radii[:, None]*self.deltar, axis=0) / np.pi
 
-        #### spot integration
+        # spot integration
 
         # Full-size spotted mask with (largest) planet disk removed
         mask_nop = mask.copy()
-        mask_nop[np.ix_(indtheta_p, indr_p)] = 0 # Assumes spot_value == 1 !!
+        mask_nop[np.ix_(indtheta_p, indr_p)] = 0  # Assumes spot_value == 1 !!
         # integration along theta
-        fraction_spot = (mask_nop.sum(0) * self.deltath)/(2.*np.pi)  # (nr) 
+        fraction_spot = (mask_nop.sum(0) * self.deltath)/(2.*np.pi)  # (nr)
         fraction_spot = fraction_spot[:, None].repeat(nw, axis=1)  # (nr, nw)
-        
+
         # spotted mask just on the rectangle containing the largest planet disk
         mask_rmax = mask[np.ix_(indtheta_p, indr_p)]
         mask_rmax = mask_rmax[:, :, None].repeat(nw, axis=2).astype(int)
         mask_rmax *= (~(mask_p.astype(bool))).astype(int)
         # integration along theta
-        fraction_spot[indr_p] += (mask_rmax.sum(0)*self.deltath)/(2.*np.pi)  ################
+        fraction_spot[indr_p] += (mask_rmax.sum(0)*self.deltath)/(2.*np.pi)
         ff_spot = np.sum(fraction_spot * 2. * np.pi *
                          self.radii[:, None] * self.deltar, axis=0) / np.pi
         return fraction_spot, fraction_planet, ff_spot, ff_planet
